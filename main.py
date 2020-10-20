b9c949ec6d570785400eb931f8d96ec49778c328
eweber69 <weberem@pedarecc.sa.edu.au>
2020-10-20 17:20:13 +1030

    Second Commit

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import jinja2
-import pandas as pd
+from flask import Flask, render_template
 import matplotlib.pyplot as plt
 import csv
-from flask import Flask, render_template
+import io
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from pandas.plotting import register_matplotlib_converters
+
+register_matplotlib_converters()
 
 # Instantiate a Flask app
 app = Flask(__name__)
@@ -14,20 +18,49 @@ def index():
     return render_template('index.html')
 
 
-def graph():
-    plt.figure(figsize=(9, 7))
+@app.route('/week')
+def past_week():
+    return render_template('week.html')
+
+
+@app.route('/History')
+def history():
+    return render_template('History.html')
+
+
+def six_august():
     x = []
     y = []
-    with open('readings.csv') as csvfile:
+    with open('2020-08-06.csv', 'r') as csvfile:
         plots = csv.reader(csvfile, delimiter=',')
+        next(plots)
         for row in plots:
-            x.append(float(row[0]))
-            y.append(float(row[1]))
-    plt.subplot(2, 1, 1)
-    plt.plot(x, y, marker='o')
+            y.append(float(row[2]))
+            x.append(float(row[1]))
+    plt.plot(x, y)
     plt.xlabel('Time of Day')
     plt.ylabel('BGL Reading')
-    plt.title('BGL Readings 1st of\nAugust')
-    plt.xticks(range(1, 32))
+    plt.title('BGL Readings 6th of\nAugust')
+    plt.gcf().autofmt_xdate()
+    plt.show()
 
-    mpld3.show()
+
+def seven_august():
+    x = []
+    y = []
+    with open('2020-08-07.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        next(plots)
+        for row in plots:
+            y.append(float(row[1]))
+            x.append(float(row[0]))
+    plt.plot(x, y)
+    plt.xlabel('Time of Day')
+    plt.ylabel('BGL Reading')
+    plt.title('BGL Readings 7th of\nAugust')
+    plt.gcf().autofmt_xdate()
+    plt.show()
+
+
+six_august()
+seven_august()
